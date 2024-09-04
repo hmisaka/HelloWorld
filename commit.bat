a6878b2521d88db07542d142eaa4834ec438191c
hmisaka <1409640444@qq.com>
2024-09-04 23:49:05 +0800

    20240904

--- commit.bat
+++ commit.bat
@@ -1,7 +1,18 @@
-set -ex
+@echo off
+REM 脚本将进行以下操作：添加更改、提交更改、推送到远程仓库
 
-git pull origin master
+REM 设置 Git 提交信息
+set /p commitMessage=请输入提交信息： 
+
+REM 添加所有更改
 git add .
-git commit -am %date%
-git push origin master
 
+REM 提交更改
+git commit -m "%commitMessage%"
+
+REM 推送到默认的远程仓库
+git push
+
+REM 打印完成消息
+echo 提交已完成！
+pause
