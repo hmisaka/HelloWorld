2186f9b378446eeec4f908b3d4f4bc936bfb85ee
hmisaka <1409640444@qq.com>
2024-09-22 16:01:47 +0800

    $(date +'m-d')

--- commit.bat
+++ commit.bat
@@ -1,10 +1,7 @@
-REM commit msg:
-set /p commitMessage: 
-
+chcp 65001
+@echo off
+cd D:\02_日常学习\HelloWorld
 git add .
-
-git commit -m "%commitMessage%"
-
+git commit -m "$(date +'%Y-%m-%d')"
 git push
-
 echo complete
