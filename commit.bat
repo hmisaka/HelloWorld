e7a109d9ae2b86ac74a59a288a00b6a6d3bd05a8
hmisaka <1409640444@qq.com>
2024-10-26 01:29:03 +0800

    $current_date

--- commit.bat
+++ commit.bat
@@ -1,9 +1,10 @@
 chcp 65001
 @echo off
 cd D:\02_日常学习\HelloWorld
-echo "请输入提交信息："
-read message
+for /f "tokens=1-4 delims=/: " %%i in ("%date% %time%") do (
+    set current_date=%%i-%%j-%%k_%%l
+)
 git add .
-git commit -m "$message"
+git commit -m "$current_date"
 git push
 echo complete
